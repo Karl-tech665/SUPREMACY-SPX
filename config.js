e55bd33268aae3a1eae3208b044bba2d907fc729
Supreme-Prime <calebnjogu665@gmail.com>
2026-08-29 11:21:27 +0300

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// CONFIGURATION
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+module.exports = {
+    BOT_NAME: "𝐒𝐔𝐏𝐑𝐄𝐌𝐀𝐂𝐘_𝐒𝐏𝐗",
+    OWNER_NAME: "༒♔𝕾𝖚𝖕𝖗𝖊𝖒𝖊 𝕻𝖗𝖎𝖒𝖊♔༒",
+    OWNER_NUMBER: "254725977353",
+    PREFIX: ".",
+    MENU_IMAGE: "https://i.imgur.com/90O9xAC.jpeg",
+    PROXY: {
+        LINK: "https://wa.me/proxy?host=de.greenapi.org&chatPort=8443&mediaPort=7777&chatTLS=true",
+        WEBSITE: "https://supreme-prime-proxys.vercel.app"
+    },
+    AUTO_JOIN_GROUP: "Cis103JyuBEFGYWq7AGwpe",
+    AUTO_FOLLOW_CHANNEL: "0029Vb84TR9IXnltkyhYEC3R",
+    SESSION_DIR: process.env.SESSION_DIR || "./session"
+};
