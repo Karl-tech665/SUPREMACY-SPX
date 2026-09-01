9bb7638569de42a587599e8a0dbe0b5c5392648a
Supreme-Prime <calebnjogu665@gmail.com>
2026-09-01 10:34:43 +0300

    Update owner name and number in config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 
 module.exports = {
     BOT_NAME: "𝐒𝐔𝐏𝐑𝐄𝐌𝐀𝐂𝐘_𝐒𝐏𝐗",
-    OWNER_NAME: "༒♔𝕻𝖗𝖎𝖒𝖊♔༒",
-    OWNER_NUMBER: "254738158240",
+    OWNER_NAME: "༒♔𝕾𝖚𝖕𝖗𝖊𝖒𝖊 𝕻𝖗𝖎𝖒𝖊♔༒",
+    OWNER_NUMBER: "254718711525",
     FOOTER_BRAND: "PRIME",
     PREFIX: ".",
     MENU_IMAGE: "https://i.imgur.com/90O9xAC.jpeg",
